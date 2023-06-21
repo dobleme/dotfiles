8ca9cb287b96751b5e764a7d14e346b8266e019f
Lluc Martorell <dev@lluc.me>
2023-06-21 14:17:39 +0200

    Removed harpoon nvim plugin

--- config/nvim/after/plugin/harpoon.lua
+++
@@ -1,7 +0,0 @@
-local mark = require("harpoon.mark")
-local ui = require("harpoon.ui")
-
-vim.keymap.set("n", "<leader>ha", mark.add_file)
-vim.keymap.set("n", "<leader>hm", ui.toggle_quick_menu)
-vim.keymap.set("n", "<leader>hn", ui.nav_next)
-vim.keymap.set("n", "<leader>hp", ui.nav_prev)
--- config/nvim/lua/dobleme/packer.lua
+++ config/nvim/lua/dobleme/packer.lua
@@ -11,7 +11,6 @@ return require('packer').startup(function(use)
 	use {
 		'nvim-treesitter/nvim-treesitter', run = ':TSUpdate' 
 	}
-	use 'theprimeagen/harpoon'
 	use 'mbbill/undotree'
 	use 'lewis6991/gitsigns.nvim'
 	use {
