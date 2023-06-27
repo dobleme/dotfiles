4705a3a0b3eae0effcabc4df25f5be45fc972ae2
Lluc Martorell <dev@lluc.me>
2023-06-27 13:04:52 +0200

    Customizin my lualine a little tiny bit

--- config/nvim/after/plugin/lualine.lua
+++ config/nvim/after/plugin/lualine.lua
@@ -2,9 +2,21 @@ local lualine = require('lualine')
 
 lualine.setup({
     options = {
-        icons_enabled = false,
-        theme = 'delta',
-        component_separators = { left = '|', right = '|'},
-        section_separators = ''
-    }
+        icons_enabled = true,
+        theme = 'fluoromachine',
+        component_separators = { left = '❫', right = '❪' },
+        section_separators = { left = '', right = '' }
+    },
+    sections = {
+        lualine_a = { 'mode' },
+        lualine_b = {
+            { 'branch', icon = '' },
+            'diff',
+            'diagnostics'
+        },
+        lualine_c = { 'filename' },
+        lualine_x = { 'encoding', 'filetype' },
+        lualine_y = { 'progress' },
+        lualine_z = { 'location' }
+    },
 })
