fa08bf40ca85980d4b21c93226f108ad92256f75
Lluc Martorell <dev@lluc.me>
2023-06-28 23:50:34 +0200

    Clean lsp config

--- config/nvim/after/plugin/lsp.lua
+++ config/nvim/after/plugin/lsp.lua
@@ -1,62 +1,45 @@
 local lsp = require('lsp-zero')
-
-lsp.preset('recommended')
-lsp.ensure_installed({
-	'bashls',
-	'cmake',
-	'dockerls',
-    'docker_compose_language_service',
-	'gopls',
-	'pyright',
-    'texlab',
-})
-
 local cmp = require('cmp')
-local cmp_select = {behavior = cmp.SelectBehavior.Select}
-local cmp_mappings = lsp.defaults.cmp_mappings({
-  ['<C-p>'] = cmp.mapping.select_prev_item(cmp_select),
-  ['<C-n>'] = cmp.mapping.select_next_item(cmp_select),
-  ['<CR>'] = cmp.mapping.confirm({ select = false }),
-  ["<C-Space>"] = cmp.mapping.complete(),
-})
+local nls = require('null-ls')
 
+lsp.preset({})
+
+lsp.defaults.cmp_mappings({
+    ['<CR>'] = cmp.mapping.confirm({ select = true, behavior = cmp.ConfirmBehavior.Replace }),
+    ["<C-a>"] = cmp.mapping.complete(),
+})
 lsp.on_attach(function(client, bufnr)
-   	local opts = {buffer = bufnr, remap = false}
-
-	vim.keymap.set("n", "gd", function() vim.lsp.buf.definition() end, opts)
-	vim.keymap.set("n", "K", function() vim.lsp.buf.hover() end, opts)
-	vim.keymap.set("n", "<leader>vws", function() vim.lsp.buf.workspace_symbol() end, opts)
-	vim.keymap.set("n", "<leader>vd", function() vim.diagnostic.open_float() end, opts)
-	vim.keymap.set("n", "[d", function() vim.diagnostic.goto_next() end, opts)
-	vim.keymap.set("n", "]d", function() vim.diagnostic.goto_prev() end, opts)
-	vim.keymap.set("n", "<leader>vca", function() vim.lsp.buf.code_action() end, opts)
-	vim.keymap.set("n", "<leader>vrr", function() vim.lsp.buf.references() end, opts)
-	vim.keymap.set("n", "<leader>vrn", function() vim.lsp.buf.rename() end, opts)
-	vim.keymap.set("i", "<C-h>", function() vim.lsp.buf.signature_help() end, opts)
-    vim.keymap.set({"n", "x"}, "<leader>cf", function()
-        vim.lsp.buf.format({async = false, timeout_ms = 10000})
+    local opts = { buffer = bufnr, remap = false }
+    vim.keymap.set("n", "<leader>vo", function() vim.lsp.buf.hover() end, opts)
+    vim.keymap.set("n", "<leader>vr", function() vim.lsp.buf.rename() end, opts)
+    vim.keymap.set("n", "<leader>vh", function() vim.lsp.buf.signature_help() end, opts)
+    vim.keymap.set("n", "<leader>vc", function() vim.lsp.buf.code_action() end, opts)
+    vim.keymap.set({ "n", "x" }, "<leader>vf", function()
+        vim.lsp.buf.format({ async = false, timeout_ms = 10000 })
     end, opts)
+
+    vim.keymap.set("i", "<C-h>", function() vim.lsp.buf.signature_help() end, opts)
+    vim.keymap.set("i", "<C-a>", function() vim.lsp.buf.code_action() end, opts)
 end)
-
 lsp.set_sign_icons({
-  error = '✘',
-  warn = '▲',
-  hint = '⚑',
-  info = '»'
+    error = '✘',
+    warn = '▲',
+    hint = '⚑',
+    info = '»'
 })
-
 lsp.configure('cmake', {
     filetypes = { 'cmake', 'make' }
 })
 
-local nls = require('null-ls')
-local sources = {
-    nls.builtins.formatting.black.with({
-        args = { "-t", "py310", "-l", "79", "--stdin-filename", "$FILENAME", "--quiet", "-" }
-    }),
-    nls.builtins.formatting.goimports
-}
-nls.setup({ debug = true, sources = sources })
+nls.setup({
+    sources = {
+        nls.builtins.formatting.black.with({
+            args = { "-t", "py310", "-l", "79", "--stdin-filename", "$FILENAME", "--quiet", "-" }
+        }),
+        nls.builtins.formatting.gofmt,
+        nls.builtins.formatting.goimports,
+        nls.builtins.diagnostics.checkmake,
+    }
+})
 
 lsp.setup()
-
