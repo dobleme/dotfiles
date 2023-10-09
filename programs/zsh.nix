c1907ac9caee5f454cf18b35fbc52f52b1db6e8a
Lluc Martorell <dev@lluc.me>
2023-10-09 18:46:08 +0200

    [wip]

--- programs/zsh.nix
+++ programs/zsh.nix
@@ -1,5 +1,22 @@
 {pkgs, ...}: {
     programs.zsh = {
         enable = true;
+
+        autocd = true;
+        shellAliases = {
+            z = "source $HOME/.zshrc";
+            s = "sudo";
+            v = "vim";
+            g = "git";
+            d = "docker";
+            "." = "cd ~";
+            ".." = "cd ..";
+            "..." = "cd ../..";
+            "--" = "-=cd -";
+            ls = "ls --color";
+            l = "ls -lha";
+            ll = "ls -lh";
+        };
+        syntaxHighlighting = true;
     };
 }
