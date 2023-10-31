4010df6f70a36b54f2f079b0b7bf3619dd58c523
Lluc Martorell <dev@lluc.me>
2023-10-31 10:44:36 +0100

    Add base16 nvim configuration colors in lua

--- programs/nvim/nvim.nix
+++ programs/nvim/nvim.nix
@@ -1,21 +1,27 @@
-{ pkgs, lib, config, nix-colors, ...}: let
-    colors-lib = nix-colors.lib.contrib { inherit pkgs; };
+{ pkgs, lib, config, ...}: let
+    colors = with config.colorScheme.colors; pkgs.substituteAll {
+        src = ./colors.lua;
+        base00 = base00; base01 = base01; base02 = base02; base03 = base03;
+        base04 = base04; base05 = base05; base06 = base06; base07 = base07;
+        base08 = base08; base09 = base09; base0A = base0A; base0B = base0B;
+        base0C = base0C; base0D = base0D; base0E = base0E; base0F = base0F;
+    };
 in {
     programs.neovim = {
         enable = true;
         defaultEditor = true;
         viAlias = true;
         vimAlias = true;
-        extraConfig = "colorscheme nix-${config.colorScheme.slug}";
         extraLuaConfig = ''
             ${builtins.readFile ./options.lua}
+            ${builtins.readFile colors}
             ${builtins.readFile ./treesitter.lua}
             ${builtins.readFile ./telescope.lua}
             ${builtins.readFile ./gitsigns.lua}
             ${builtins.readFile ./whichkey.lua}
         '';
         plugins = with pkgs.vimPlugins; [
-            (colors-lib.vimThemeFromScheme { scheme = config.colorScheme; } )
+            nvim-base16
             telescope-nvim
             gitsigns-nvim
             which-key-nvim
