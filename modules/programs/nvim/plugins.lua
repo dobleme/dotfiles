5e4027f7c08a53d275aabe9981bae66ac3b1ed7d
Lluc Martorell <7830854+dobleme@users.noreply.github.com>
2024-09-16 12:25:08 +0200

    [wip]

--- modules/programs/nvim/plugins.lua
+++ modules/programs/nvim/plugins.lua
@@ -1,70 +1,69 @@
-require('nvim-treesitter.configs').setup({
+require("nvim-treesitter.configs").setup({
     highlight = { enable = true },
     indent = { enable = true }
 })
 
-require('gitsigns').setup({
+require("gitsigns").setup({
     signcolumn = true,
     numhl = true,
     current_line_blame = true
 })
 
+require("which-key").setup({
+    marks = false,
+    registers = true,
+    spelling = { enabled = false },
+    presets = {
+        operators = true,
+        motions = true,
+        text_objects = true,
+        windows = true,
+        nav = true,
+        z = true,
+        g = true,
+    },
+    win = { border = "single" },
+    icons = { mappings = false },
+})
 require('which-key').add({
+    { "<leader>pv", desc = "Open netrw" },
+    { "<leader>y", desc = "Copy to xclip" },
     { "<leader>f", group = "telescope" },
-    { "<leader>fS", desc = "Find workspace symbols" },
     { "<leader>fb", desc = "Find current buffers" },
     { "<leader>fc", desc = "Find incoming calls" },
     { "<leader>fd", desc = "Find/go symbol definitions" },
+    { "<leader>fe", desc = "Find diagnostics" },
     { "<leader>ff", desc = "Find files" },
     { "<leader>fg", desc = "Live grep in workspace" },
     { "<leader>fi", desc = "Find/go symbol implementations" },
     { "<leader>fo", desc = "Find outgoing calls" },
+    { "<leader>fq", desc = "Find quickfix list" },
     { "<leader>fr", desc = "Find symbol references" },
     { "<leader>fs", desc = "Find document symbols" },
+    { "<leader>fS", desc = "Find workspace symbols" },
     { "<leader>v", group = "lsp" },
-    { "<leader>vc", desc = "Code action" },
-    { "<leader>vf", desc = "Format document" },
-    { "<leader>vh", desc = "Signature help symbol" },
-    { "<leader>vo", desc = "Hover symbol" },
-    { "<leader>vr", desc = "Rename symbol" },
+    { "<leader>vd", desc = "Open diagnostic float" },
+    { "<leader>vj", desc = "Goto next diagnostic" },
+    --{ "<leader>vc", desc = "Code action" },
+    --{ "<leader>vf", desc = "Format document" },
+    --{ "<leader>vh", desc = "Signature help symbol" },
+    --{ "<leader>vo", desc = "Hover symbol" },
+    --{ "<leader>vr", desc = "Rename symbol" },
 })
 
 local ts = require('telescope.builtin')
 local th = require('telescope.themes')
-vim.keymap.set('n', '<leader>ff', function()
-    ts.find_files({ follow = true })
-end)
-vim.keymap.set('n', '<leader>fb', function()
-    ts.buffers(th.get_dropdown())
-end)
-vim.keymap.set('n', '<leader>fg', function()
-    ts.live_grep(th.get_dropdown())
-end)
-vim.keymap.set('n', '<leader>fs', function()
-    ts.lsp_document_symbols(th.get_dropdown())
-end)
-vim.keymap.set('n', '<leader>fS', function()
-    ts.lsp_workspace_symbols(th.get_dropdown())
-end)
-vim.keymap.set('n', '<leader>fc', function()
-    ts.lsp_incoming_calls(th.get_ivy({ layout_config = { height = 20 } }))
-end)
-vim.keymap.set('n', '<leader>fo', function()
-    ts.lsp_outgoing_calls(th.get_ivy({ layout_config = { height = 20 } }))
-end)
-vim.keymap.set('n', '<leader>fr', function()
-    ts.lsp_references(th.get_ivy({ layout_config = { height = 20 } }))
-end)
-vim.keymap.set('n', '<leader>fi', function()
-    ts.lsp_implementations(th.get_ivy({ layout_config = { height = 20 } }))
-end)
-vim.keymap.set('n', '<leader>fd', function()
-    ts.lsp_definitions(th.get_ivy({ layout_config = { height = 20 } }))
-end)
-vim.keymap.set('n', '<leader>fq', function()
-    ts.quickfix(th.get_dropdown())
-end)
-vim.keymap.set('n', '<leader>fe', function()
-    ts.diagnostics(th.get_ivy({ layout_config = { height = 30 } }))
-end)
-
+local ivyTh = th.get_ivy({ layout_config = { height = 20 } })
+local dropdownTh = th.get_dropdown()
+vim.keymap.set('n', '<leader>fb', function() ts.buffers(dropdownTh) end)
+vim.keymap.set('n', '<leader>fc', function() ts.lsp_incoming_calls(ivyTh) end)
+vim.keymap.set('n', '<leader>fd', function() ts.lsp_definitions(ivyTh) end)
+vim.keymap.set('n', '<leader>fe', function() ts.diagnostics(theme) end)
+vim.keymap.set('n', '<leader>ff', function() ts.find_files({ follow = true }) end)
+vim.keymap.set('n', '<leader>fg', function() ts.live_grep(dropdownTh) end)
+vim.keymap.set('n', '<leader>fi', function() ts.lsp_implementations(ivyTh) end)
+vim.keymap.set('n', '<leader>fo', function() ts.lsp_outgoing_calls(ivyTh) end)
+vim.keymap.set('n', '<leader>fq', function() ts.quickfix(dropdownTh) end)
+vim.keymap.set('n', '<leader>fr', function() ts.lsp_references(ivyTh) end)
+vim.keymap.set('n', '<leader>fs', function() ts.lsp_document_symbols(dropdownTh) end)
+vim.keymap.set('n', '<leader>fS', function() ts.lsp_workspace_symbols(dropdownTh) end)
